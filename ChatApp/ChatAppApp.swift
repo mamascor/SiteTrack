e3a0bfe4949fc4d3c812cf4e9a4dad539ee4cf72
Marco Mascorro <Marco.mascorroa@icloud.com>
2023-05-22 15:59:02 -0500

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Marco Mascorro on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
